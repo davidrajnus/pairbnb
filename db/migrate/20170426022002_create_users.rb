48a7ff758e1b5254ea9630004fe85be9d47c303a
DRAJ <davidrajnus@gmail.com>
2017-04-26 11:42:32 +0800

    clearance gem completed

--- db/migrate/20170426022002_create_users.rb
+++ db/migrate/20170426022002_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :first_name
+      t.string :last_name
       t.timestamps null: false
       t.string :email, null: false
       t.string :encrypted_password, limit: 128, null: false
